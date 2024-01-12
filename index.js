044931deb3ed6f142e4bf2dc2597097226e5f7cd
Josh Saterdalen <josh@saterdalen.me>
2024-01-12 13:05:35 -0600

    init package

---
+++ index.js
@@ -0,0 +1,82 @@
+import fs from 'fs';
+import papa from 'papaparse';
+import xml2js from 'xml2js';
+
+async function parseXML() {
+    const filePath = './force-app/main/default/settings/Address.settings-meta.xml';
+    // read file Address.settings-meta.xml and parse it
+    const file = fs.readFileSync(filePath);
+    const parser = new xml2js.Parser();
+    const result = await parser.parseStringPromise(file);
+
+    // get the countries and states
+    const countriesAndStates = result.AddressSettings.countriesAndStates[0].countries;
+
+    // create an array of states
+    const states = [];
+    countriesAndStates.forEach((country) => {
+        if (country.states) {
+            country.states.forEach((state) => {
+                states.push(
+                    new State(
+                        state.active[0],
+                        state.integrationValue[0],
+                        state.isoCode[0],
+                        state.label[0],
+                        state.standard[0],
+                        state.visible[0],
+                        country.label[0],
+                        country.isoCode[0]
+                    )
+                );
+            });
+        }
+    });
+
+    const countries = [];
+    countriesAndStates.forEach((country) => {
+        countries.push(
+            new Country(
+                country.active[0],
+                country.integrationValue[0],
+                country.isoCode[0],
+                country.label[0],
+                country.standard[0],
+                country.visible[0]
+            )
+        );
+    });
+
+    // create csv files
+    const csv = papa.unparse(states);
+    fs.writeFileSync('./states.csv', csv);
+
+    const csv2 = papa.unparse(countries);
+    fs.writeFileSync('./countries.csv', csv2);
+}
+
+parseXML();
+
+class State {
+    constructor(active, integrationValue, isoCode, label, standard, visible, country, countryCode) {
+        this.active = active;
+        this.integrationValue = integrationValue;
+        this.isoCode = isoCode;
+        this.label = label;
+        this.standard = standard;
+        this.visible = visible;
+        this.country = country;
+        this.countryCode = countryCode;
+    }
+}
+
+class Country {
+    constructor(active, integrationValue, isoCode, label, standard, visible) {
+        this.active = active;
+        this.integrationValue = integrationValue;
+        this.isoCode = isoCode;
+        this.label = label;
+        this.standard = standard;
+        this.visible = visible;
+    }
+}
---
+++ readme.md
@@ -0,0 +1,53 @@
+# pryfile.js
+
+[![NPM Version](http://img.shields.io/npm/v/sf-state-parser)](https://www.npmjs.org/package/st-state-parser)
+
+## Installation
+
+### Dependencies
+
+- Node.js (verify by running `node -v`)
+- an SFDX project
+
+### Setup
+
+Install with npm
+
+```bash
+  npm i -g sf-state-parser
+```
+
+## Usage
+
+sf-state-parser is to be used from within an SFDX project directory. It's easiest to open a project in VS Code, then open the embedded terminal (`ctr`/`cmd`+`j`).
+
+### Download State and Country/Territory Picklist data
+
+Use the sfdx CLI or Org Browser in VS Code to get your picklist data.
+> You will need to have the CLI or a VS Code project already authenticated with the org you want to get data from!
+
+#### Option #1: CLI
+
+Run the following in the terminal from within your sfdx project:
+
+```bash
+    sf project retrieve start -m Settings:Address
+```
+
+#### Option #2: Org Browser
+
+Use the Org Browser tab in VS Code to retrieve "Address" from Settings.
+
+![org browser](images/org-browser.png)
+
+You should now have the file `Address.settings-meta.xml` in `force-app/main/default/Settings`
+
+### Run it
+
+Run the following command in your terminal:
+
+```bash
+    sf-state
+```
+
+It will create two CSV files in your project root: `states.csv` and `countries.csv`. You can import them into Excel from here.
